0e5f763642ac03dd32efb4f1103825bc4d3dd45e
Nicolas Porto <111699513+NicolasPorto@users.noreply.github.com>
2023-12-05 08:58:21 -0300

    fix: termos.

--- orion-bank-web/src/pages/TermosPolitica/index.jsx
+++ orion-bank-web/src/pages/TermosPolitica/index.jsx
@@ -22,24 +22,40 @@ const TermosPolitica = () => {
                                         <div>
                                             <div className="container-texto-termos">
                                                 <div className="termos-texto">
-                                                    <label>
-                                                        Lorem ipsum dolor sit amet. Sit quia aliquam sit ipsa aliquid
-                                                        vel dolore earum id rerum error est atque aliquam 33 assumenda
-                                                        repellendus sit omnis distinctio! Vel animi aliquid non sapiente
-                                                        doloribus vel explicabo delectus aut quis ipsum quo cupiditate
-                                                        nemo eos pariatur eaque ut similique amet.
-                                                        Eum officia quae est dolorem quos ea voluptatem quasi. Qui ipsa
-                                                        dolorum ut sunt quia sit dicta tenetur et fugiat laborum et nesciunt
-                                                        pariatur. Qui dolorem voluptates quo similique explicabo ut iste temporibus
-                                                        At sapiente labore qui nihil nulla. Nam fuga debitis ea quisquam rerum et
-                                                        ipsam architecto qui voluptatem nulla sed veniam ipsum non nobis facilis.
-                                                        Et eius dicta id dicta Quis ut pariatur consectetur et itaque tenetur in
-                                                        Quis autem a veniam galisum in doloremque impedit. Qui cupiditate tempore
-                                                        quo harum atque est obcaecati consequatur aut expedita voluptatibus ad dolorem
-                                                        soluta est tenetur quos non adipisci alias. Et eaque repellat rem incidunt
-                                                        quia in quisquam dolorum a quia quis ut dolor numquam vel ipsa enim est
-                                                        necessitatibus eius!
-                                                    </label>
+                                                    <div>
+                                                        <p>
+                                                            <strong>1. Introdução</strong><br />
+                                                            Bem-vindo ao Orion Bank, um banco digital que oferece uma variedade de serviços financeiros, incluindo PIX, PIX QR Code e transferências bancárias. Ao utilizar nossos serviços, você concorda com os seguintes termos e políticas.
+                                                        </p>
+                                                        <p>
+                                                            <strong>2. Serviços Oferecidos</strong><br />
+                                                            <strong>2.1. PIX (Sistema de Pagamentos Instantâneos):</strong> O Orion Bank oferece a opção de realizar transações instantâneas por meio do PIX, permitindo transferências de fundos em tempo real.<br />
+                                                            <strong>2.2. PIX QR Code:</strong> Nosso serviço PIX QR Code permite que você receba pagamentos escaneando um código QR gerado pelo Orion Bank.<br />
+                                                            <strong>2.3. Transferências Bancárias:</strong> Facilitamos transferências entre contas do Orion Bank e outras instituições financeiras, seguindo os procedimentos e regulamentações estabelecidas para cada tipo de transferência.
+                                                        </p>
+
+                                                        <p>
+                                                            <strong>3. Segurança e Privacidade</strong><br />
+                                                            <strong>3.1. Proteção de Dados:</strong> O Orion Bank prioriza a segurança e privacidade dos seus dados. Utilizamos medidas de segurança avançadas para proteger as informações dos nossos clientes.<br />
+                                                            <strong>3.2. Confidencialidade:</strong> Respeitamos a confidencialidade das informações dos clientes e não compartilhamos dados pessoais sem o consentimento prévio, a menos que exigido por lei.<br />
+                                                        </p>
+
+                                                        <p>
+                                                            <strong>4. Responsabilidades do Cliente</strong><br />
+                                                            <strong>4.1. Segurança da Conta:</strong> É responsabilidade do cliente manter suas credenciais de acesso (senha, PIN etc.) em segredo para evitar acesso não autorizado à sua conta.<br />
+                                                            <strong>4.2. Uso Adequado dos Serviços:</strong> O cliente concorda em utilizar os serviços do Orion Bank de maneira legal e ética, evitando atividades fraudulentas ou ilegais.<br />
+                                                        </p>
+
+                                                        <p>
+                                                            <strong>5. Disponibilidade dos Serviços</strong><br />
+                                                            <strong>5.1. Manutenção Programada:</strong> O Orion Bank pode realizar manutenções programadas nos sistemas. Faremos o possível para minimizar o impacto nos serviços, informando os clientes sobre qualquer interrupção prevista.<br />
+                                                        </p>
+
+                                                        <p>
+                                                            <strong>6. Alterações nos Termos e Políticas</strong><br />
+                                                            <strong>6.1. Atualizações:</strong> O Orion Bank se reserva o direito de fazer alterações nos termos e políticas. Quaisquer mudanças serão comunicadas aos clientes através dos canais oficiais do banco.<br />
+                                                        </p>
+                                                    </div>
                                                 </div>
                                             </div>
 
